8692e43ac46a2608fedca654d215c90121382525
Raaaaaaaa <1637666464@qq.com>
2021-09-16 11:10:18 +0800

    to test

---
+++ src/test/java/com/zfl/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
